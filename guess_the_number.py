6da62a6fcab9f5e7ce563e3a69d8fb7bd114291f
Shloka <shlokathaker@gmail.com>
2026-07-01 17:26:04 +0530

    Implement number guessing game

---
+++ guess_the_number.py
@@ -0,0 +1,23 @@
+import random
+
+number = random.randint(1,30)
+
+x = int(input("Guess a number from 1 to 30:"))
+    
+while True:
+    if x==number:
+         print("BOOM! You guessed it right!🎉")
+         break
+    elif x>number:
+         print("Your guess is too high.")
+    else:
+         print("Your guess is too low.")
+
+    print("1. Try again")
+    print("2. Ragequit")
+    y = input("What would you like to do?:")
+    if y=="1":
+        x = int(input("Guess the number again:"))
+    else:
+        print(f"The number was {number}! LOL you suck!😆")
+        break
