cd2d59fbd1d8cf0feea56b6f5b796ef22633b1a5
Shloka <shlokathaker@gmail.com>
2026-07-17 18:57:54 +0530

    Add comment about f-string functionality
    Added a comment explaining f-string usage.

--- hello.py
+++ hello.py
@@ -1,2 +1,3 @@
 name = input("What is your name? ")
 print(f"Hello, {name}! Welcome to my python journey!")
+#f string considers/reads the expressions directly by placing them inside {}, making the code simpler
