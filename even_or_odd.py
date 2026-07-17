5a5ecebd36feab2f23682ccf92d5fdae60698029
Shloka <shlokathaker@gmail.com>
2026-07-17 19:00:21 +0530

    Add even or odd checker in even_or_odd.py
    Implement a program to check if a number is even or odd.

---
+++ even_or_odd.py
@@ -0,0 +1,7 @@
+#learn the use of ' % ' in python. It means 'reminder' and is used to check if a number is perfectly divisible by given number or not
+
+number = int(input("Enter a number:"))
+if number%2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
